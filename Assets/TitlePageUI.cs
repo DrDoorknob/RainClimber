01d1feffad0de6054c992be8c19b24f57517310d
David Keyworth <katananokage@gmail.com>
2017-04-23 14:50:00 -0400

    Fade in animation on title screen

--- Assets/TitlePageUI.cs
+++ Assets/TitlePageUI.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitlePageUI : MonoBehaviour {
+
+    public Animator screenAnimator;
+    public Image fadeOverPanel;
+
+    public float fadeInTime;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +18,15 @@ public class TitlePageUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Time.time < fadeInTime) {
+            var color = fadeOverPanel.color;
+            color.a = 1f - (Time.time / fadeInTime);
+            fadeOverPanel.color = color;
+        }
+        else if (fadeOverPanel.color.a != 0f)
+        {
+            fadeOverPanel.color = new Color(0f, 0f, 0f, 0f);
+        }
 	}
 
     public void NewGame()
